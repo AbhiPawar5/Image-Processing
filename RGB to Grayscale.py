4e0b24c41627d5627dacab05ef3d8762155824e1
Abhishek Pawar <45484556+AbhiPawar5@users.noreply.github.com>
2018-12-20 21:53:15 +0530

    Update RGB to Grayscale.py

--- RGB to Grayscale.py
+++ RGB to Grayscale.py
@@ -2,11 +2,10 @@
 import cv2,numpy as np
 
 #Absolute path of image to be read. 0 is passed to read it directly as Grayscale image
-img = cv2.imread("C:\\Users\\Abhishek Pawar\\Desktop\\myImage.jpg")
-grayimg = cv2.imread("C:\\Users\\Abhishek Pawar\\Desktop\\myImage.jpg",cv2.IMREAD_GRAYSCALE)
+path = "C:\\Users\\Abhishek Pawar\\Desktop\\"
+grayimg = cv2.imread(path + "myImage.jpg",cv2.IMREAD_GRAYSCALE)
 
 #function to show images
-cv2.imshow("Sample Image",img)
 cv2.imshow("Grayscale Image",grayimg)
 
 #wait for user to press any key and destroy all windows
